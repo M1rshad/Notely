f90b0afcd046d94f64fa6114027150a6d1874283
M1rshad <abdullamirshadcl@gmail.com>
2024-10-14 22:37:54 +0530

    first commit

---
+++ backend/Note/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+
+    CATEGORY = (
+        ('BUSINESS','Business'),
+        ('PERSONAL','Personal'),
+        ('IMPORTANT','Important'),
+    )
+
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    slug = models.SlugField(unique=True)
+    category = models.CharField(max_length=15, choices=CATEGORY, default='PERSONAL')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug_base = slugify(self.title)
+            slug = slug_base
+
+            if Note.objects.filter(slug=slug).exists():
+                slug = f'{slug_base}-{get_random_string(5)}'
+            self.slug = slug
+
+        super(Note, self).save(*args, **kwargs)
